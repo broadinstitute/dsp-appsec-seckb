b3e8aa9385ab19d823bdaf0516b704bd718053fe
Albano Drazhi <adrazhi@broadinstitute.org>
2020-01-22 14:18:43 -0500

    clean up script

--- gitbook.sh
+++
@@ -1,3 +0,0 @@
-cd security-kb-gitbook
-docker run -v "$PWD:/gitbook" -p 4000:4000 billryan/gitbook:base gitbook serve
-docker run --rm -v "$PWD:/gitbook" -p 4000:4000 billryan/gitbook gitbook serve
